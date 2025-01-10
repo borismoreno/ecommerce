a4bbd07ec629259c4862c8ac8f7b970c66a735de
Boris Moreno <borismoreno@Boriss-MacBook-Pro.local>
2025-01-10 14:38:27 -0500

    call turso

---
+++ src/turso.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@libsql/client/web';
+
+export const turso = createClient({
+    url: import.meta.env.PUBLIC_TURSO_DATABASE_URL,
+    authToken: import.meta.env.PUBLIC_TURSO_AUTH_TOKEN,
+});
