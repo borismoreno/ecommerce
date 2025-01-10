f8707d5a0b7ea90cbcf2064bd1edc53005febda4
Boris Moreno <borismoreno@Boriss-MacBook-Pro.local>
2025-01-10 14:51:34 -0500

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import awsAmplify from 'astro-aws-amplify';
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    adapter: awsAmplify(),
-    output: 'server'
+  adapter: awsAmplify(),
+  output: 'server',
+  integrations: [tailwind()]
 });
